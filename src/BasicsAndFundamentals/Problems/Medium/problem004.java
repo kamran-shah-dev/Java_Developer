d151b48f04cf5d089baf6bf40f938e2102947c6d
kamran-shah-dev <mrbukhari117@gmail.com>
2026-08-14 15:34:09 +0500

    Problem004 is a medium level problem of java basics and fundamentals

---
+++ src/BasicsAndFundamentals/Problems/Medium/problem004.java
@@ -0,0 +1,30 @@
+package BasicsAndFundamentals.Problems.Medium;
+
+import java.util.Scanner;
+
+public class problem004 {
+    public static void main(String[] args) {
+        // determine if a year is leap year or not
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a year: ");
+        int year = input.nextShort();
+
+        boolean leapYear = year % 4 == 0;
+
+        if(year <= 0) {
+            System.out.println("Year cannot be negative or zero");
+        } else {
+            if (year % 100 == 0) {
+                leapYear = year % 400 == 0;
+            }
+
+            if (leapYear) {
+                System.out.println(year + " is a leap year");
+            } else {
+                System.out.println(year + " is not a leap year");
+            }
+        }
+    }
+}
