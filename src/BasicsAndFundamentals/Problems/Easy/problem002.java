aa89af6871ec80e415180f5bc9c96b51ab36e585
kamran-shah-dev <mrbukhari117@gmail.com>
2026-08-14 14:34:43 +0500

    problem 2 of basics and fundamentals of java programming

---
+++ src/BasicsAndFundamentals/Problems/Easy/problem002.java
@@ -0,0 +1,44 @@
+package BasicsAndFundamentals.Problems.Easy;
+
+import javax.xml.transform.Source;
+import java.util.Scanner;
+
+public class problem002 {
+    public static void main(String[] args) {
+        /*
+            Given an integer, determine whether it is positive/negative/zero, even/odd,
+            and divisible by both 3 and 5.
+        */
+
+        // Take an integer as input
+
+        // Scanner is a class that is used to take input from user
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int x = input.nextInt();
+
+        // check if divisible by 3 and 5
+        if (x % 3 == 0 && x % 5 == 0) {
+            System.out.println(x + " is divisible by 3 and 5 both.");
+        } else {
+            System.out.println(x + " is not divisible by 3 and 5 both.");
+        }
+
+        // check if even or odd (parity)
+        if (x % 2 == 0) {
+            System.out.println(x + " is even.");
+        } else {
+            System.out.println(x + " is odd.");
+        }
+
+        // check the sign
+        if (x == 0) {
+            System.out.println(x + " is zero.");
+        } else if (x < 0) {
+            System.out.println(x + " is negative.");
+        } else {
+            System.out.println(x + " is positive.");
+        }
+    }
+}
