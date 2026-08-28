0241ad2ac9ee3553ca5275a9f958e4923e781d07
kamran-shah-dev <mrbukhari117@gmail.com>
2026-08-28 13:19:33 +0500

    Completing lab with constructors and encapsulation

--- src/OOP/Labs/BookAccess.java
+++ src/OOP/Labs/BookAccess.java
@@ -1,12 +1,21 @@
 package OOP.Labs;
 
+import java.util.Objects;
 import java.util.Scanner;
 
-class Book {
+class Book implements Cloneable{
     private String bookTitle;
     private String bookAuthor;
     private float bookPrice;
 
+    Book () {
+        this.bookAuthor = this.bookTitle = null;
+    }
+    Book (String bookTitle, String bookAuthor, float bookPrice) {
+        this.bookTitle = bookTitle;
+        this.bookAuthor = bookAuthor;
+        this.bookPrice = bookPrice;
+    }
     public void setBookTitle(String bookTitle) {
         this.bookTitle = bookTitle;
     }
@@ -38,17 +47,38 @@ class Book {
                """.formatted(bookTitle, bookAuthor, bookPrice);
         // here we can see a %.2f (.2) with float, that is just to limit the decimal values after point to 2
     }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Float.compare(bookPrice, book.bookPrice) == 0 && Objects.equals(bookTitle, book.bookTitle)
+                && Objects.equals(bookAuthor, book.bookAuthor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookTitle, bookAuthor, bookPrice);
+    }
 }
+
 public class BookAccess {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
         Scanner input = new Scanner(System.in);
         Book[] Books = new Book[10];
-        System.out.println("- MENU - \n(1) -> View all books\n(2) -> Add a book \n(3) -> To Compare Book Prices\n(Any key) -> To Exit");
+        System.out.println("- MENU - \n(1) -> View all books\n(2) -> Add a book \n(3) -> To Compare Book Prices");
+        System.out.println("(4) -> Compare Books\n(5) -> Clone a Book\n(Any key) -> To Exit");
 
         int bookIndex = 0;
         String bookTitle, bookAuthor;
         float bookPrice;
         String userChoice;
+        int bookNumberOne, bookNumberTwo;
         label:
         while (true) {
             System.out.print("What operation you want to perform: ");
@@ -61,33 +91,90 @@ public class BookAccess {
                     if (bookIndex == 10) {
                         System.out.println("No more space to add BOOKS!!");
                     } else {
-                        System.out.print("Enter book title: ");
-                        bookTitle = input.nextLine();
-                        System.out.print("Enter book title: ");
-                        bookAuthor = input.nextLine();
-                        System.out.print("Enter book title: ");
-                        bookPrice = Float.parseFloat(input.nextLine());
-                        Book book = new Book();
-                        book.setBookTitle(bookTitle);
-                        book.setBookAuthor(bookAuthor);
-                        book.setBookPrice(bookPrice);
-                        Books[bookIndex++] = book;
-                        System.out.println("Book added successfully");
+                        System.out.println("How do you want to add the books?");
+                        System.out.println("All book attributes at once or attributes one by one");
+                        System.out.print("To enter at once press 6 and to enter one by one enter 7: ");
+                        userChoice = input.nextLine();
+                        if (userChoice.equals("6")) {
+                            System.out.print("Enter book title: ");
+                            bookTitle = input.nextLine();
+                            System.out.print("Enter book title: ");
+                            bookAuthor = input.nextLine();
+                            System.out.print("Enter book title: ");
+                            bookPrice = Float.parseFloat(input.nextLine());
+                            Books[bookIndex++] = new Book(bookTitle, bookAuthor, bookPrice);
+                            System.out.println("Book added successfully");
+                        } else if (userChoice.equals("7")) {
+                            Book book = new Book();
+                            System.out.print("Enter book title: ");
+                            bookTitle = input.nextLine();
+                            System.out.println("Book Title Added");
+                            book.setBookTitle(bookTitle);
+                            System.out.print("Enter book title: ");
+                            bookAuthor = input.nextLine();
+                            book.setBookAuthor(bookAuthor);
+                            System.out.println("Book Author Added");
+                            System.out.print("Enter book title: ");
+                            bookPrice = Float.parseFloat(input.nextLine());
+                            book.setBookPrice(bookPrice);
+                            System.out.println("Book Price Added");
+                            Books[bookIndex++] = book;
+                        } else {
+                            System.out.println("Invalid Input");
+                        }
                     }
                     break;
                 case "3":
                     printAllBooks(Books);
                     System.out.println("Which two books you want to compare?");
                     System.out.print("First Book Number: ");
-                    int bookNumberOne = Integer.parseInt(input.next());
+                    bookNumberOne = Integer.parseInt(input.nextLine());
                     System.out.print("Second Book Number: ");
-                    int bookNumberTwo = Integer.parseInt(input.next());
-                    compareBookPrice(Books[bookNumberOne - 1], Books[bookNumberTwo - 1]);
+                    bookNumberTwo = Integer.parseInt(input.nextLine());
+                    if (bookNumberOne == 0 || bookNumberOne > filledBucketSize(Books)
+                            || bookNumberTwo == 0 || bookNumberTwo > filledBucketSize(Books)) {
+                        System.out.println("Incorrect Book Numbers");
+                    } else {
+                        compareBookPrice(Books[bookNumberOne - 1], Books[bookNumberTwo - 1]);
+                    }
+                    break;
+                case "4":
+                    System.out.print("First Book Number: ");
+                    bookNumberOne = Integer.parseInt(input.nextLine());
+                    System.out.print("Second Book Number: ");
+                    bookNumberTwo = Integer.parseInt(input.nextLine());
+                    if (bookNumberOne == 0 || bookNumberOne > filledBucketSize(Books)
+                            || bookNumberTwo == 0 || bookNumberTwo > filledBucketSize(Books)) {
+                        System.out.println("Incorrect Book Numbers");
+                    } else {
+                        if (compareBooks(Books[bookNumberOne - 1] , Books[bookNumberTwo - 1])) {
+                            System.out.println(Books[bookNumberOne - 1]);
+                            System.out.println(Books[bookNumberTwo - 1]);
+                            System.out.println("Books are same");
+                        } else {
+                            System.out.println("Books are not same");
+                        }
+                    }
+                    break;
+                case "5":
+                    printAllBooks(Books);
+                    System.out.print("Enter the number of book you want to clone: ");
+                    bookNumberOne = Integer.parseInt(input.nextLine());
+                    if (bookNumberOne == 0 || bookNumberOne > filledBucketSize(Books)) {
+                        System.out.println("No Books To Clone");
+                    } else {
+                        Books[bookIndex++] = (Book) Books[bookNumberOne - 1].clone();
+                        System.out.println("Book Clones Successfully");
+                    }
                     break;
                 default:
                     break label;
             }
         }
+    }
+
+    private static boolean compareBooks(Book book1, Book book2) {
+        return book1.equals(book2);
     }
 
     private static void compareBookPrice(Book book1, Book book2) {
@@ -114,4 +201,6 @@ public class BookAccess {
         }
         return index;
     }
+
+
 }
