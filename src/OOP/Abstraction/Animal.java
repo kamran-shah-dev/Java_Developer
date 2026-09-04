da6d67de8130e424dd24ca6fde7e7eb8f5266e4c
kamran-shah-dev <mrbukhari117@gmail.com>
2026-09-04 11:00:37 +0500

    Implementing the animal farm scenario using abstract classes.

---
+++ src/OOP/Abstraction/Animal.java
@@ -0,0 +1,80 @@
+package OOP.Abstraction;
+
+abstract public class Animal {
+    private String name;
+    private String food;
+
+    public Animal (String name) {
+        this.name = name;
+    }
+
+    abstract public String Sound();
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public String getFood() {
+        return this.food;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                "sound='" + Sound() + '\''+
+                "Eats Food='" + getFood() + '\''+
+                '}';
+    }
+}
+
+class Dog extends Animal {
+    public Dog (String name) {
+        super(name);
+    }
+
+    @Override
+    public String Sound() {
+        return "Bow! Bow!";
+    }
+
+    @Override
+    public void setFood(String food) {
+        super.setFood(food);
+    }
+}
+
+class Cat extends Animal {
+    public Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public String Sound() {
+        return "Meow Meow Meow";
+    }
+
+    @Override
+    public void setFood(String food) {
+        super.setFood(food);
+    }
+
+}
+
+class Cow extends Animal {
+    public Cow(String name) {
+        super(name);
+    }
+
+    @Override
+    public String Sound() {
+        return "Moo!   Moo!";
+    }
+
+    @Override
+    public void setFood(String food) {
+        super.setFood(food);
+    }
+}
+
+
